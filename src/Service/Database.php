319370e8b243442da34addaa44ac8e3064fca4b0
NSG <nacerasahed@hotmail.com>
2021-12-20 10:57:15 +0100

    add argument to database

--- src/Service/Database.php
+++ src/Service/Database.php
@@ -10,24 +10,32 @@ use PDO;
 
  class Database
   {  
-    private $host = 'localhost';
     private $dbname = 'projet_5';
+    private $host = 'localhost';
     private $username = 'root';
     private $password = 'root';
     private $port = '8889';
+    private $pdo;
 
-    private function database(){
-    try {
-
-    $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $port);
-  
-    echo "Connecté à $dbname sur $host avec succès.";
-  
-  } 
-  
-  catch (PDOException $e) 
+    public function __construct(string $dbname, string $host, string $username ,string $password, string $port)
     {
-  die("Impossible de se connecter à la base de données $dbname :" . $e->getMessage());
+      $this->dbname = $dbname;
+      $this->host = $host;
+      $this->username = $username;
+      $this->password = $password;
+      $this->port = $port;
     }
-  }
+
+    public function getPDO():PDO
+    {
+      if ($this->pdo === null){
+          $this->pdo = $this->pdo = new PDO ("mysql:dbname={$this->dbname};host={$this->host}",$this->username,$this->password,$this->port);
+      }
+    
+    return $this->pdo;
 }
+}
+
+/* PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+*/
