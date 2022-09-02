2efdbb38231a7e9fe1023b98055f889ebd9c40a5
Nsahed <nacerasahed@hotmail.com>
2022-09-02 14:43:05 +0200

    user session update

--- src/Service/Http/Session/Session.php
+++ src/Service/Http/Session/Session.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 namespace App\Service\Http\Session;
+use App\Model\Entity\User;
 
 final class Session
 {
@@ -16,7 +17,7 @@ final class Session
 
     public function set(string $name, mixed $value): void
     {
-        $this->sessionParamBag[$name] = $value;
+        $this->sessionParamBag[$name] = $value; //objet user
     }
 
     public function get(string $name): mixed
@@ -46,5 +47,39 @@ final class Session
         $this->remove('flashes');
 
         return $flashes;
+    }
+    
+    public function isAdmin(User $user): bool
+    {
+        if ($user->getRole() === false || $user->getRole() === 0 || $user->getROle() === '0') {
+            return false;
+        }
+        return true;
+    }
+
+    public function isCurrentUserAdmin(): bool
+    {
+        if ($this->isLoggedIn()) {
+            $user = $this->userManager->read($this->session->get('id_user'));
+            $role = $user->getRole();
+            if ($role === false || $role === 0 || $role === '0') {
+                return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public function getCurrentUser(): User
+    {
+        return $this->userManager->read($this->session->get('id_user'));
+    }
+
+    public function isDisabled(User $user): bool
+    {
+        if ($user->getDeleted() === true || $user->getDeleted() === 1 ||  $user->getDeleted() === '1') {
+            return true;
+        }
+        return false;
     }
 }
